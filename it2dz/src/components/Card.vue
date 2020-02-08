79479320f443eb9c2b91ed457773460a1d11236f
Olga <rammsteinlifada@mail.ru>
2020-02-08 12:22:26 +1000

    add 2 dz

---
+++ it2dz/src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container" style = "">
+        <div class="row">
+            <div class="col">
+                {{ this.card.name }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+              <img :src="this.card.im">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                {{ this.card.description }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <router-link to="/">back</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            card: []
+        }
+    },
+    mounted() {
+        this.card = this.$store.getters.CARDS.findIndex(x => x.code == this.$router.params.id);
+    }
+}
+</script>
+<style>
+    div {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e51;
+        margin-top: 10px;
+ 
+    }
+</style>
---
+++ it2dz/src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class = "row">
+          <div v-for="product in this.products" :key="product.card">
+              <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="product-details">
+                  <img :src = product.im :click>
+                  <h3>{{product.name}}</h3>
+                  <p>
+                    {{product.description}}
+                  </p>
+                  <p>
+                      {{product.price}}
+                  </p>
+                </div>
+              </div>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+    mounted() {
+        this.products = this.$store.getters.CARDS;
+    }
+}
+</script>
